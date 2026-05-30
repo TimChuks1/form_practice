874d5046accfdbf47ca8e05ed4f815187249ec4b
TimChuks1 <chukwukatimothy1@gmail.com>
2026-05-30 01:59:15 +0100

    added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -11,14 +12,16 @@ mongoose
   .catch(() => console.error("Error connecting MongoDB"));
 
 const app = express();
+const _dirname = path.resolve();
 const Port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client")));
 app.use(cors());
 app.use("/api", router);
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.post("/submit", (req, res) => {
